19f3b844bb19e514de121b87b182fccd694e4e11
Pygano <106460951+Pygano@users.noreply.github.com>
2022-07-17 16:10:35 -0500

    Add files via upload

---
+++ calculadora/ProyectoAguila_Calculadora_JoseArias.py
@@ -0,0 +1,36 @@
+#Operaciones con números enteros
+
+numero1 = int (input("Introduzca el primer número: "))
+numero2 = int (input("Intriduzca el sefundo número: "))
+
+#Suma
+suma = numero1 + numero2
+print ("La suma de los dos números es: ", suma)
+print ("La suma de ", numero1, "y el ", numero2, "es igual a: ", suma)
+
+#resta
+resta = numero1 - numero2
+print ("La resta de ", numero1, "y el ", numero2, "es igual a: ", resta)
+
+#multiplicación
+multi = numero1 * numero2
+print ("La multiplicación de ", numero1, "y el ", numero2, "es igual a: ", multi)
+
+#division entera
+division1 = numero1 // numero2
+print ("La división entera de ", numero1, "y el ", numero2, "es igual a: ", division1)
+
+#division normal
+division2 = numero1 / numero2
+print ("La división de ", numero1, "y el ", numero2, "es igual a: ", division2)
+
+#resto o residuo
+resto = numero1 % numero2
+print ("El resto o residuo de ", numero1, "y el ", numero2, "es igual a: ", resto)
+
+print ("FIN...")
+
+
+
+
+
