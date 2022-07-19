ec61a3a0ea741b0416909183ae9ee2bbfdac5a63
Pygano <106460951+Pygano@users.noreply.github.com>
2022-07-19 11:45:24 -0500

    Add files via upload

---
+++ calculadora/ProyectoGate_Calculadora_AngelGarcia_version01.py
@@ -0,0 +1,26 @@
+print("Calculadora de operaciones básicas")
+print("----------------------------------")
+
+
+print("Ingresa el primer número")
+a = int(input())
+print("Ingresa el segundo número")
+b = int(input())
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+
+print("")
+print("")
+
+print(a,"+",b,"=",c)
+print(a,"-",b,"=",d)
+print(a,"*",b,"=",e)
+print(a,"/",b,"=",f)
+
+print("")
+print("")
+
+
+print("Fin")
