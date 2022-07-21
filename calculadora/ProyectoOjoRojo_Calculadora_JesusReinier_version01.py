e031ca405fb9bb3382e0c21fd48a681bb5dc1a37
Pygano <106460951+Pygano@users.noreply.github.com>
2022-07-20 20:34:53 -0500

    Add files via upload

---
+++ calculadora/ProyectoOjoRojo_Calculadora_JesusReinier_version01.py
@@ -0,0 +1,20 @@
+
+num1 = int(input('Escribe el número 1: '))
+num2 = int(input('Escribe el número 2: '))
+
+print('El primer número es: ',num1)
+print('El segundo número es: ',num2)
+
+suma=num1+num2
+resta=num1-num2
+multiplicacion=num1*num2
+division=num1/num2
+modulo=num1%num2
+
+print ('========Resultados:========')
+print ('Suma:',suma)
+print ('Resta:',resta)
+print ('Multiplicación:',multiplicacion)
+print ('División:',division)
+print ('Módulo:',modulo)
+
