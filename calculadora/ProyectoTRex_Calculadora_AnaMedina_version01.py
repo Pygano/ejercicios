ac607f37c4932b44721063e15ca3e753acf473b1
Pygano <106460951+Pygano@users.noreply.github.com>
2022-07-20 20:33:43 -0500

    Add files via upload

---
+++ calculadora/ProyectoTRex_Calculadora_AnaMedina_version01.py
@@ -0,0 +1,16 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+residuo = numero1%numero2
+
+print("#"*50)
+print(f"La suma de los dos numeros es: {suma}")
+print(f"La resta de los dos numeros es: {resta}")
+print(f"La multiplicación de los dos numeros es: {multiplicacion}")
+print(f"La división de los dos numeros es: {division}")
+print(f"El residuo de la división es: {residuo}")
+print("#"*50)
