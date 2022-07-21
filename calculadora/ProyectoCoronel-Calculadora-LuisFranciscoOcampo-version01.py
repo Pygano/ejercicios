19c93a7afd485e374d97c20db85e582fcc106f85
Pygano <106460951+Pygano@users.noreply.github.com>
2022-07-20 22:34:09 -0500

    Add files via upload

---
+++ calculadora/ProyectoCoronel-Calculadora-LuisFranciscoOcampo-version01.py
@@ -0,0 +1,15 @@
+# operaciones aritmeticas
+print("Ingrese un número: ")
+n1 = float(input())
+print("Ingrese otro número: ")
+n2 = float(input())
+suma = n1 + n2
+resta = n1 - n2 
+mult = n1 * n2
+div = n1 / n2
+res = n1 % n2
+print("La suma es igual a: ",suma)
+print("La resta es igual a: ",resta)
+print("La multiplicación es igual a:",mult)
+print("La división es igual a: ", div)
+print("El residuo es igual a: ",res)
